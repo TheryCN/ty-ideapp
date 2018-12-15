da72ec3322a4c876a42d8f9273d9c6106a3c1929
Théry Charasson <thery.charasson@gmail.com>
2018-12-15 17:46:08 +0100

    Add Idea actions

--- ty-ideapp-react-front/src/components/Idea.js
+++ ty-ideapp-react-front/src/components/Idea.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import IdeaBackground from '../assets/idea-background.jpg';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import AddIcon from '@material-ui/icons/Add';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 import './Idea.css';
 
@@ -15,6 +20,17 @@ class Idea extends Component {
       if(idea) {
       return (
         <div className="idea" style={ideaBackgroundStyle}>
+          <div className="actions">
+            <IconButton aria-label="Edit Idea">
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="Add Idea">
+              <AddIcon />
+            </IconButton>
+            <IconButton aria-label="Delete Idea">
+              <DeleteIcon />
+            </IconButton>
+          </div>
           <div container className="idea-content">
             <div className="title-content">
               <span className="title">{idea.name}</span>
