9c955200d90c6604b55309eb70b89b721797046a
tcharasson <thery.charasson@gmail.com>
2019-02-06 13:55:42 +0100

    Add PropTypes

--- ty-ideapp-react-front/src/components/IdeaView.js
+++ ty-ideapp-react-front/src/components/IdeaView.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import moment from 'moment';
 import Rating from './Rating';
 
@@ -11,9 +13,21 @@ const IdeaView = ({ idea }) => (
     <div>{idea.description}</div>
     <Rating value={idea.rating} max={5} readOnly={true} />
     <div>{idea.feasibility}</div>
-    <div>{moment(idea.createdOn).format('LLL')}</div>
+    <div>{idea.createdOn}</div>
     <div>{moment(idea.updatedOn).format('LLL')}</div>
   </div>
 )
+
+IdeaView.propTypes = {
+  idea: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    rating: PropTypes.number,
+    feasibility: PropTypes.string,
+    createdOn: PropTypes.string,
+    updatedOn: PropTypes.string
+  }).isRequired
+};
 
 export default IdeaView;
