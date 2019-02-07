71ae71d42ba81a09e805bbd28ff2f870794575b5
tcharasson <thery.charasson@gmail.com>
2019-02-07 14:52:15 +0100

    Update IdeaSave.java

--- ty-ideapp-dto/src/main/java/com/github/therycn/tyideapp/IdeaSave.java
+++ ty-ideapp-dto/src/main/java/com/github/therycn/tyideapp/IdeaSave.java
@@ -1,5 +1,7 @@
 package com.github.therycn.tyideapp;
 
+import java.util.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,18 +15,22 @@ import lombok.Setter;
 @Setter
 public class IdeaSave {
 
-    private Long id;
+	private Long id;
 
-    private String name;
+	private String name;
 
-    private String subTitle;
+	private String subTitle;
 
-    private String description;
+	private String description;
 
-    private int rating;
+	private int rating;
 
-    private String feasibility;
+	private String feasibility;
 
-    private Long workspaceId;
+	private Long workspaceId;
+
+	private Date createdOn;
+
+	private Date updatedOn;
 
 }
