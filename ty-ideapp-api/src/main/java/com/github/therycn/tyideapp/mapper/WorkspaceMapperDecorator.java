145e2f8cfef106845c8cbce5636409dfa744af66
tcharasson <thery.charasson@gmail.com>
2019-02-08 16:55:15 +0100

    Update WorkspaceMapperDecorator.java

--- ty-ideapp-api/src/main/java/com/github/therycn/tyideapp/mapper/WorkspaceMapperDecorator.java
+++ ty-ideapp-api/src/main/java/com/github/therycn/tyideapp/mapper/WorkspaceMapperDecorator.java
@@ -5,13 +5,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.util.CollectionUtils;
 
 import com.github.therycn.tyideapp.WorkspaceListItem;
+import com.github.therycn.tyideapp.WorkspaceSave;
+import com.github.therycn.tyideapp.entity.User;
 import com.github.therycn.tyideapp.entity.Workspace;
+import com.github.therycn.tyideapp.repository.WorkspaceRepository;
 
 public abstract class WorkspaceMapperDecorator implements WorkspaceMapper {
 
 	@Autowired
 	@Qualifier("delegate")
 	private WorkspaceMapper delegate;
+
+	@Autowired
+	private WorkspaceRepository workspaceRepo;
 
 	/*
 	 * (non-Javadoc)
@@ -29,6 +35,28 @@ public abstract class WorkspaceMapperDecorator implements WorkspaceMapper {
 		}
 
 		return workspaceListItem;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.github.therycn.tyideapp.mapper.WorkspaceMapper#to(com.github.therycn.
+	 * tyideapp.WorkspaceSave, com.github.therycn.tyideapp.entity.User)
+	 */
+	@Override
+	public Workspace to(WorkspaceSave workspaceSave, User user) {
+		Workspace workspaceUpdate = delegate.to(workspaceSave, user);
+
+		Workspace workspace;
+		if (workspaceSave.getId() != null) {
+			workspace = workspaceRepo.findById(workspaceSave.getId()).orElse(workspaceUpdate);
+			workspace.setName(workspaceUpdate.getName());
+		} else {
+			workspace = workspaceUpdate;
+		}
+
+		return workspace;
 	}
 
 }
