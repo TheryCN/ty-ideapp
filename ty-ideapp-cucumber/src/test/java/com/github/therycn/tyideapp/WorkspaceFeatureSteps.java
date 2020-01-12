0172af9af4db02536063b31ef1dd113584979326
Théry Charasson <thery.charasson@gmail.com>
2020-01-12 11:12:13 +0100

    Correction des tests cucumber

--- ty-ideapp-cucumber/src/test/java/com/github/therycn/tyideapp/WorkspaceFeatureSteps.java
+++ ty-ideapp-cucumber/src/test/java/com/github/therycn/tyideapp/WorkspaceFeatureSteps.java
@@ -27,95 +27,95 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WorkspaceFeatureSteps {
 
-    private String baseUri;
+	private String baseUri;
 
-    private TestRestTemplate restTemplate = new TestRestTemplate();
+	private TestRestTemplate restTemplate = new TestRestTemplate();
 
-    private HttpHeaders headers;
+	private HttpHeaders headers;
 
-    private ResponseEntity<WorkspaceListItem[]> response = null;
+	private ResponseEntity<WorkspaceListItem[]> response = null;
 
-    private WorkspaceSave workspaceSave;
+	private WorkspaceSave workspaceSave;
 
-    private Long selectedWorkspaceId;
+	private Long selectedWorkspaceId;
 
-    @Given("authenticate as {string}")
-    public void authenticate_as(String user) {
-        initProperties();
-        headers = createHeaders(user, "ChangeIt");
-    }
+	@Given("authenticate as {string}")
+	public void authenticate_as(String user) {
+		initProperties();
+		headers = createHeaders(user, "ChangeIt");
+	}
 
-    @When("I ask for workspaces")
-    public void i_ask_for_workspaces() {
-        response = restTemplate.exchange(baseUri + "/workspace/", HttpMethod.GET, new HttpEntity<Object>(headers),
-                WorkspaceListItem[].class);
-    }
+	@When("I ask for workspaces")
+	public void i_ask_for_workspaces() {
+		response = restTemplate.exchange(baseUri + "/workspaces/", HttpMethod.GET, new HttpEntity<Object>(headers),
+				WorkspaceListItem[].class);
+	}
 
-    @Then("I should have the following workspaces:")
-    public void i_should_have_the_following_workspaces(List<String> expectedAnswers) {
-        WorkspaceListItem[] body = response.getBody();
-        for (int i = 0; i < expectedAnswers.size(); i++) {
-            Assertions.assertThat(body[i].getName()).isEqualTo(expectedAnswers.get(i));
-        }
-    }
+	@Then("I should have the following workspaces:")
+	public void i_should_have_the_following_workspaces(List<String> expectedAnswers) {
+		WorkspaceListItem[] body = response.getBody();
+		for (int i = 0; i < expectedAnswers.size(); i++) {
+			Assertions.assertThat(body[i].getName()).isEqualTo(expectedAnswers.get(i));
+		}
+	}
 
-    @Given("I want to create a workspace named {string}")
-    public void i_want_to_create_a_workspace_named(String name) {
-        workspaceSave = new WorkspaceSave();
-        workspaceSave.setName(name);
-    }
+	@Given("I want to create a workspace named {string}")
+	public void i_want_to_create_a_workspace_named(String name) {
+		workspaceSave = new WorkspaceSave();
+		workspaceSave.setName(name);
+	}
 
-    @When("I ask for saving workspace")
-    public void i_ask_for_saving_workspace() {
-        ResponseEntity<WorkspaceListItem> response = restTemplate.exchange(baseUri + "/workspace/", HttpMethod.POST,
-                new HttpEntity<Object>(workspaceSave, headers), WorkspaceListItem.class);
-        selectedWorkspaceId = response.getBody().getId();
-    }
+	@When("I ask for saving workspace")
+	public void i_ask_for_saving_workspace() {
+		ResponseEntity<WorkspaceListItem> response = restTemplate.exchange(baseUri + "/workspaces/", HttpMethod.POST,
+				new HttpEntity<Object>(workspaceSave, headers), WorkspaceListItem.class);
+		selectedWorkspaceId = response.getBody().getId();
+	}
 
-    @Given("I want to update it with new name {string}")
-    public void i_want_to_update_it_with_new_name(String name) {
-        workspaceSave = new WorkspaceSave();
-        workspaceSave.setId(selectedWorkspaceId);
-        workspaceSave.setName(name);
-    }
+	@Given("I want to update it with new name {string}")
+	public void i_want_to_update_it_with_new_name(String name) {
+		workspaceSave = new WorkspaceSave();
+		workspaceSave.setId(selectedWorkspaceId);
+		workspaceSave.setName(name);
+	}
 
-    @Given("an existing workspace named {string}")
-    public void an_existing_workspace_named(String name) {
-        workspaceSave = new WorkspaceSave();
-        workspaceSave.setName(name);
-        ResponseEntity<WorkspaceListItem> response = restTemplate.exchange(baseUri + "/workspace/", HttpMethod.POST,
-                new HttpEntity<Object>(workspaceSave, headers), WorkspaceListItem.class);
-        selectedWorkspaceId = response.getBody().getId();
-    }
+	@Given("an existing workspace named {string}")
+	public void an_existing_workspace_named(String name) {
+		workspaceSave = new WorkspaceSave();
+		workspaceSave.setName(name);
+		ResponseEntity<WorkspaceListItem> response = restTemplate.exchange(baseUri + "/workspaces/", HttpMethod.POST,
+				new HttpEntity<Object>(workspaceSave, headers), WorkspaceListItem.class);
+		selectedWorkspaceId = response.getBody().getId();
+	}
 
-    @When("I delete the selected workspace")
-    public void i_delete_the_selected_workspace() {
-        restTemplate.exchange(baseUri + "/workspaces/" + selectedWorkspaceId, HttpMethod.DELETE,
-                new HttpEntity<Object>(headers), Void.class);
-    }
+	@When("I delete the selected workspace")
+	public void i_delete_the_selected_workspace() {
+		restTemplate.exchange(baseUri + "/data-rest/workspaces/" + selectedWorkspaceId, HttpMethod.DELETE,
+				new HttpEntity<Object>(headers), Void.class);
+	}
 
-    private HttpHeaders createHeaders(String username, String password) {
-        return new HttpHeaders() {
-            /** Serial version. */
-            private static final long serialVersionUID = -4968652194130326824L;
+	private HttpHeaders createHeaders(String username, String password) {
+		return new HttpHeaders() {
+			/** Serial version. */
+			private static final long serialVersionUID = -4968652194130326824L;
 
-            {
-                String auth = username + ":" + password;
-                byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
-                String authHeader = "Basic " + new String(encodedAuth);
-                set("Authorization", authHeader);
-            }
-        };
-    }
+			{
+				String auth = username + ":" + password;
+				byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodedAuth);
+				set("Authorization", authHeader);
+			}
+		};
+	}
 
-    public void initProperties() {
-        try {
-            Properties properties = new Properties();
-            properties.load(getClass().getClassLoader().getResourceAsStream("application.properties"));
-            baseUri = properties.getProperty("base.uri");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	public void initProperties() {
+		try {
+			Properties properties = new Properties();
+			properties.load(getClass().getClassLoader().getResourceAsStream("application.properties"));
+			baseUri = properties.getProperty("base.uri");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 
 }
