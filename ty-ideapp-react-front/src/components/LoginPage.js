710f8d8baf5e0d831a13865f7e02d4f09986ccbb
Théry Charasson <thery.charasson@gmail.com>
2020-01-12 19:46:37 +0100

    Update backgrounds

--- ty-ideapp-react-front/src/components/LoginPage.js
+++ ty-ideapp-react-front/src/components/LoginPage.js
@@ -3,17 +3,22 @@ import Grid from '@material-ui/core/Grid';
 
 import Login from '../containers/Login.js';
 import Registration from '../containers/Registration.js';
+import './LoginPage.css';
 
 const LoginPage = () => {
+  const ideaBackgroundStyle = {
+    background: "url(/images/idea-background-1.jpg) no-repeat fixed center",
+    backgroundSize: "cover"
+  };
   return (
-    <div>
+    <div style={ideaBackgroundStyle}>
       <Grid container className="layout">
         <Grid key={1} item xs={3} sm={3}>
         </Grid>
-        <Grid key={2} item xs={3} sm={3}>
+        <Grid key={2} item xs={3} sm={3} className="login-element">
           <Login />
         </Grid>
-        <Grid key={3} item xs={3} sm={3}>
+        <Grid key={3} item xs={3} sm={3} className="login-element">
           <Registration />
         </Grid>
         <Grid key={4} item xs={3} sm={3}>
