f2b2571b5963fc20782fcf31c8d6b05c1feb3212
Théry Charasson <thery.charasson@gmail.com>
2021-06-17 08:36:27 +0200

    Update UserRestController.java

--- ty-ideapp/ty-ideapp-api/src/main/java/com/github/therycn/tyideapp/controller/user/UserRestController.java
+++ ty-ideapp/ty-ideapp-api/src/main/java/com/github/therycn/tyideapp/controller/user/UserRestController.java
@@ -1,17 +1,5 @@
 package com.github.therycn.tyideapp.controller.user;
 
-import javax.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.github.therycn.tyideapp.UserInfo;
 import com.github.therycn.tyideapp.UserPasswordUpdate;
 import com.github.therycn.tyideapp.UserRegistration;
@@ -21,59 +9,61 @@ import com.github.therycn.tyideapp.exception.validation.UserWrongOldPasswordExce
 import com.github.therycn.tyideapp.exception.validation.UsernameAlreadyExistsException;
 import com.github.therycn.tyideapp.mapper.UserMapper;
 import com.github.therycn.tyideapp.service.UserService;
-
 import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * User Rest Controller.
- * 
- * @author tcharass
- *
  */
 @RestController
 @RequestMapping("/users")
 public class UserRestController {
 
-	private UserMapper userMapper;
+    private final UserMapper userMapper;
 
-	private UserService userService;
+    private final UserService userService;
 
-	public UserRestController(UserMapper userMapper, UserService userService) {
-		this.userMapper = userMapper;
-		this.userService = userService;
-	}
+    public UserRestController(UserMapper userMapper, UserService userService) {
+        this.userMapper = userMapper;
+        this.userService = userService;
+    }
 
-	@ApiOperation(value = "Get user details")
-	@GetMapping("/")
-	public ResponseEntity<UserInfo> getLoggedUser(Authentication authentication) {
-		User user = (User) authentication.getPrincipal();
-		return ResponseEntity.ok(userMapper.to(user));
-	}
+    @ApiOperation(value = "Get user details")
+    @GetMapping("/")
+    public ResponseEntity<UserInfo> getLoggedUser(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(userMapper.to(user));
+    }
 
-	@ApiOperation(value = "User registration")
-	@PostMapping("/")
-	public ResponseEntity<UserInfo> register(@Valid @RequestBody UserRegistration userRegistration)
-			throws UsernameAlreadyExistsException {
-		// Map to a user object
-		User userToCreate = userMapper.to(userRegistration);
+    @ApiOperation(value = "User registration")
+    @PostMapping("/")
+    public ResponseEntity<UserInfo> register(@Valid @RequestBody UserRegistration userRegistration)
+            throws UsernameAlreadyExistsException {
+        // Map to a user object
+        User userToCreate = userMapper.to(userRegistration);
 
-		// Save it with functionals checks
-		return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.to(userService.create(userToCreate)));
-	}
+        // Save it with functionals checks
+        return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.to(userService.create(userToCreate)));
+    }
 
-	@ApiOperation(value = "Update user password")
-	@PatchMapping("/password")
-	public ResponseEntity<UserInfo> updatePassword(@Valid @RequestBody UserPasswordUpdate passwordUpdate,
-			Authentication authentication) throws UserWrongOldPasswordException, UserNotFoundException {
-		// Retrieve logged user
-		User user = (User) authentication.getPrincipal();
+    @ApiOperation(value = "Update user password")
+    @PatchMapping("/password")
+    public ResponseEntity<UserInfo> updatePassword(@Valid @RequestBody UserPasswordUpdate passwordUpdate,
+                                                   Authentication authentication) throws UserWrongOldPasswordException, UserNotFoundException {
+        // Retrieve logged user
+        User user = (User) authentication.getPrincipal();
 
-		if (!userService.checkOldPasswordEq(user, passwordUpdate.getOldPassword())) {
-			throw new UserWrongOldPasswordException();
-		}
+        if (!userService.checkOldPasswordEq(user, passwordUpdate.getOldPassword())) {
+            throw new UserWrongOldPasswordException();
+        }
 
-		User updatedUser = userService.updatePassword(user.getId(), passwordUpdate.getNewPassword());
-		return ResponseEntity.ok(userMapper.to(updatedUser));
-	}
+        User updatedUser = userService.updatePassword(user.getId(), passwordUpdate.getNewPassword());
+        return ResponseEntity.ok(userMapper.to(updatedUser));
+    }
 
 }
